445413e02f912b2219c04da736453c80fc08c5ce
Akshaya-Muthu <akshayammuthu@gmail.com>
2025-08-11 22:36:45 +0530

    updated nodemailer

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,12 +1,13 @@
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer';
 
+// Create a transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",   // Brevo SMTP server
-  port: 587,                       // Brevo SMTP port
-  secure: false,                  // must be false for port 587
+  host: 'smtp-relay.brevo.com', // Brevo SMTP server
+  port: 587,                     // Brevo SMTP port
+  secure: false,                 // false for port 587, true for port 465
   auth: {
-    user: "942c91001@smtp-brevo.com",  // your Brevo SMTP login
-    pass: "7C05h9HG62mYWtqw",          // your Brevo SMTP password
+    user: process.env.SMTP_USER, // Use environment variable for SMTP user
+    pass: process.env.SMTP_PASS,  // Use environment variable for SMTP password
   },
 });
 
