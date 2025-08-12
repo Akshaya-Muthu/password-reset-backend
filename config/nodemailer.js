bd4d198ee72fbb918edc21df0229a2dbc48140c9
Akshaya-Muthu <akshayammuthu@gmail.com>
2025-08-12 14:55:03 +0530

    updated the reset password

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -3,8 +3,7 @@ import nodemailer from 'nodemailer';
 // Create a transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
   host: 'smtp-relay.brevo.com', // Brevo SMTP server
-  port: 587,                     // Brevo SMTP port
-  secure: false,                 // false for port 587, true for port 465
+  port: 587,                     // Brevo SMTP port                // false for port 587, true for port 465
   auth: {
     user: process.env.SMTP_USER, // Use environment variable for SMTP user
     pass: process.env.SMTP_PASS,  // Use environment variable for SMTP password
